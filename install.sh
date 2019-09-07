7e4d6fdb3dedf023ec83f4ae815fd3fb0957f49f
Ethan Marsh <emarsh55@gmail.com>
2019-09-07 16:17:37 -0700

    Add exec perms

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Set src and target dir (MacOS)
-sf_mono_dir="$( cd "$( dirname "$0" )" && pwd )"
-fonts_dir="$HOME/Library/Fonts"
+SF_MONO_DIR="$( cd "$( dirname "$0" )" && pwd )"
+FONTS_DIR="$HOME/Library/Fonts"
 
 # Copy sf mono to fonts dir
 echo "Installing fonts..."
-find "$sf_mono_dir" \( -name "SF-Mono*.otf" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$fonts_dir/"
+find "$SF_MONO_DIR" \( -name "SF-Mono*.otf" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$FONTS_DIR/"
 
 # Confirmation message
-echo "SF Mono installed to $fonts_dir"
+echo "SF Mono installed to $FONTS_DIR"
