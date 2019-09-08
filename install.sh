b7bf3fffe989a2b8b87e8cbd34bf62fc63df5c2d
Ethan Marsh <emarsh55@gmail.com>
2019-09-07 18:55:58 -0700

    Add cleanup option

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ fonts_dir="$HOME/Library/Fonts"
 
 # Copy sf mono to fonts dir
 echo "Installing SF Mono..."
-find "$sf_mono_dir" \( -name "SF-Mono*.otf" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "${fonts_dir}/"
+find "$sf_mono_dir" -name "*.otf" -type f -print0 | xargs -0 -n1 -I % cp "%" "$fonts_dir/"
 
 # Print confirmation message
-echo "SF Mono installed to $fonts_dir"
+echo "done: SF Mono installed to ${fonts_dir}"
 
-# Ask for removal of sf-mono dir
-read -p "Do you want to remove ${sf_mono_dir}? (Y/N) > "
+# Optional clean up after installation
+read -p "done: Do you want to remove ${sf_mono_dir}? (Y/N) > "
 if [[ "$REPLY" =~ ^[yY] ]]; then
     rm -rf $sf_mono_dir
     echo "Removed ${sf_mono_dir}/"
